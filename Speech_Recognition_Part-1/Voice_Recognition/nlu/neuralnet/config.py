df430f0f7da7b6acd0c300a4fbec68e2ca888a48
1932912 <1932912@brunel.ac.uk>
2022-09-30 13:26:54 +0100

    Update config.py

--- Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/config.py
+++ Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/config.py
@@ -6,3 +6,11 @@ IS_DATASET_PATH  = '../nlu_dataset/is_dataset.csv'
 MODEL_PATH = '/Volumes/My Passport 1/models/epoch50_best_model.pth' 
 TRACE_MODEL_PATH ='/Volumes/My Passport 1/models/epoch50_best_model_trace.pth' 
 LOG_PATH = '/home2/ncwn67/A-Hackers-AI-Voice-Assistant/Voice_Recognition/nlu/tb_logs/run4'
+
+#Additional Parameters:
+SAVE_MODEL = True
+#Hyper-parameters
+MAX_LEN = 61+2
+TRAIN_BATCH_SIZE = 128
+TEST_BATCH_SIZE = 128
+EPOCHS = 50
