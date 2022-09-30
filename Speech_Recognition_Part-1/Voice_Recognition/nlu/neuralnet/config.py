e81380488652ebb956c48636ddae808412fbc940
1932912 <1932912@brunel.ac.uk>
2022-09-30 13:25:32 +0100

    neuralnet config start

---
+++ Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/config.py
@@ -0,0 +1,8 @@
+import transformers
+import torch
+RAW_DATASET_PATH = '../nlu_dataset/nlu.csv'
+ER_DATASET_PATH  = '../nlu_dataset/er_dataset.csv'
+IS_DATASET_PATH  = '../nlu_dataset/is_dataset.csv'
+MODEL_PATH = '/Volumes/My Passport 1/models/epoch50_best_model.pth' 
+TRACE_MODEL_PATH ='/Volumes/My Passport 1/models/epoch50_best_model_trace.pth' 
+LOG_PATH = '/home2/ncwn67/A-Hackers-AI-Voice-Assistant/Voice_Recognition/nlu/tb_logs/run4'
