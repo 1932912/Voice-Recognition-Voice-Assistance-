cc64c1f31a734fb3ffead6c2b683d38f80b935f9
1932912 <1932912@brunel.ac.uk>
2022-09-30 13:34:02 +0100

    Update dataset.py

--- Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/dataset.py
+++ Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/dataset.py
@@ -50,4 +50,56 @@ class IntentScenarioDataset:
                 'token_type_ids':None
             }
             
+            class EntityDataset:
+    '''
+    Dataset class for entity recognition
+    '''
+    def __init__(self, text, entity):
+        self.texts = text
+        self.entity = entity
+        self.tokenizer = config.TOKENIZER
+        self.max_len = config.MAX_LEN 
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self,item):
+        text = self.texts[item]
+        entity = self.entity[item]
+
+        ids = []
+        target_entity = []
+        for i,word in enumerate(text):
+            token_ids = self.tokenizer.encode(word,
+                                          add_special_tokens=False)
+            word_piece_entity = [entity[i]]*len(token_ids)
+
+            ids.extend(token_ids)
+            target_entity.extend(word_piece_entity)
+
+        #adujst to ids and target_entity to max_len, as max_len special_tokens inclusive
+        ids = ids[:self.max_len-2]
+        target_entity = target_entity[:self.max_len-2]
+        #add cls token
+        ids = [101] + ids + [102]
+        target_entity = [0] + target_entity + [0]
+        
+        #create mask and token_type_id
+        mask,token_type_id = [1]*len(ids),[0]*len(ids)
+
+        #padding
+        padding_len = self.max_len - len(ids)
+        
+        ids = ids + ([0] * padding_len)
+        target_entity = target_entity + ([0] * padding_len)
+        mask = mask + ([0] * padding_len)
+        token_type_id = token_type_id + ([0] * padding_len)
+
+        return {
+            'ids': torch.tensor(ids,dtype=torch.long),
+            'target_entity': torch.tensor(target_entity,dtype=torch.long),
+            'mask': torch.tensor(mask,dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_id,dtype=torch.long)
+        }
+
   
