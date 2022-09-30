09dc9d75f28167641ecf9b395fe26593eea53f19
1932912 <1932912@brunel.ac.uk>
2022-09-30 13:32:57 +0100

    Update dataset.py

--- Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/dataset.py
+++ Speech_Recognition_Part-1/Voice_Recognition/nlu/neuralnet/dataset.py
@@ -13,4 +13,41 @@ class IntentScenarioDataset:
         self.require_text = require_text
         self.tokenizer = config.TOKENIZER
         self.max_len = config.MAX_LEN
+       
+    def __len__(self):
+        return len(self.texts)
     
+    def __getitem__(self,item):
+        text = self.texts[item]
+        intent = self.intent[item]
+        scenario = self.scenario[item]
+
+        ids, mask, token_type_ids = None, None, None
+        if self.require_text:
+            out = self.tokenizer.encode_plus(
+                text,
+                None,
+                add_special_token=True,
+                max_length = self.max_len,
+                padding = 'max_length'
+            )
+                        ids = out['input_ids']
+            mask = out['attention_mask']
+            token_type_ids = out['token_type_ids']
+            return {
+                'ids': torch.tensor(ids,dtype=torch.long),
+                'target_intent': torch.tensor(intent,dtype=torch.long),
+                'target_scenario': torch.tensor(scenario,dtype=torch.long),
+                'mask': torch.tensor(mask,dtype=torch.long),
+                'token_type_ids': torch.tensor(token_type_ids,dtype=torch.long)
+            }
+                   else:
+            return {
+                'ids': None,
+                'target_intent': torch.tensor(intent,dtype=torch.long),
+                'target_scenario': torch.tensor(scenario,dtype=torch.long),
+                'mask': None,
+                'token_type_ids':None
+            }
+            
+  
